e2f35a03e05d6fe551d5418b9afc882dbbd413b9
KwonBoWon <bogod1207@naver.com>
2021-03-05 13:38:31 +0000

    list

---
+++ main.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+      strs=[]
+      for char in s:
+        if char.isalnum():
+          strs.append(char.lower())
+
+      while len(strs)>1:
+        if strs.pop(0) !=strs.pop():
+          return False
+      return True
+
+
+Palindrome=Solution()
+strs=input()
+
+
+print(Solution.isPalindrome(Palindrome,strs))
+print(Palindrome.isPalindrome(strs))
+
