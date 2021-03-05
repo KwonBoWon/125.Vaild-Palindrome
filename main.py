8c5b5d492c390b3a5d563454c34931e299539beb
KwonBoWon <bogod1207@naver.com>
2021-03-05 13:55:15 +0000

    slicing

--- main.py
+++ main.py
@@ -1,21 +1,9 @@
-
-from collections import Deque
-
-
 class Solution:
-    def isPalindrome(self, s: str) -> bool:
-      strs:Deque=collections.deque()
+  def isPalindrome(self, s: str) -> bool:
+    s=s.lower()
+    s=re.sub('[^a-z0-9]', '',s)
+    return s==s[::-1]
 
-      for char in s:
-        if char.isalnum():
-          strs.append(char.lower())
-
-      while len(strs) > 1:
-        if strs.popleft() != strs.pop():
-          return False
-
-
-      return True
 
 
 Palindrome=Solution()
